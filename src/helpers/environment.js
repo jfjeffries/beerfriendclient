aa5797477d4e582b5058b4d361200441fbbf1b30
jfjeffries <jon.f.jeffries@gmail.com>
2018-07-03 14:45:41 -0400

    changed environment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,7 +3,10 @@ let APIURL = ''
 switch (window.location.hostname){
     case 'localhost':
         APIURL = 'http://localhost:3000';
+        APIURL = ''
         break;
+    case 'jfjbeerlistclient.herokuapp.com':
+        APIURL = 'https://jfj-beerfriend.herokuapp.com'
 }
 
 export default APIURL
